2e9106411b3719d73dd33a52580232961f53e1ad
eduams <eduardo4ms@gmail.com>
2025-03-23 00:15:29 -0300

    .

--- stackoverflow.py
+++ stackoverflow.py
@@ -5,6 +5,7 @@ import sqlite3
 
 conexao = sqlite3.connect("dados.db")
 cursor = conexao.cursor()
+cursor.execute("DROP TABLE IF EXISTS posts")
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
